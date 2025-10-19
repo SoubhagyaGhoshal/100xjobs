b55efcdb07ab424424501fe9e4527a5b14f23905
Soubhagya Ghoshal <141400812+SoubhagyaGhoshal@users.noreply.github.com>
2025-10-19 11:11:50 +0530

    Fix ALLOWED_HOSTS environment variable handling

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-c3_5wyyibpdm0&e-zfa0^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','*', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Add deployment platform domains
 if not DEBUG:
